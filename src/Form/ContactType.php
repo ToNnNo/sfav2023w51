4abb1199ae4167bd07f0e4c8ecab17217f32e25c
Stéphane Menut <smenut@dawan.fr>
2023-12-20 09:39:34 +0100

    fix html

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -15,15 +15,9 @@ class ContactType extends AbstractType
         $builder
             ->add('lastname', options: [
                 'label' => 'form.user.lastname.label',
-                'row_attr' => [
-                    'class' => "mb-0"
-                ]
             ])
             ->add('firstname', options: [
                 'label' => 'form.user.firstname.label',
-                'row_attr' => [
-                    'class' => "mb-0"
-                ]
             ])
             ->add('email', options: [
                 'label' => 'form.user.email.label',
