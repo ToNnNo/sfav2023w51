75fc9019d20fd6af74247f459bf8291788d70ae1
Stéphane Menut <smenut@dawan.fr>
2023-12-20 14:47:45 +0100

    Http_client

---
+++ src/Controller/HttpRequestController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+#[Route('/http', name: 'http_')]
+class HttpRequestController extends AbstractController
+{
+    public function __construct(
+        private readonly HttpClientInterface $httpClient
+    ){}
+
+    #[Route('', name: 'index')]
+    public function index(): Response
+    {
+        $users = [];
+
+        try {
+            $response = $this->httpClient->request('GET', 'https://jsonplaceholder.typicode.com/users', [
+                'auth_bearer' => "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+            ]);
+            $users = $response->toArray();
+        } catch (TransportExceptionInterface|ClientExceptionInterface|DecodingExceptionInterface|RedirectionExceptionInterface|ServerExceptionInterface $e) {
+            $this->addFlash('danger', "Une erreur est survenue ....");
+        }
+
+        return $this->render('http_request/index.html.twig', [
+            'users' => $users
+        ]);
+    }
+}
